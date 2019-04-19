f107e97d193feffe77a84a797c48d1b9535bd0ad
jzhang005 <jzhang005@citymail.cuny.edu>
2019-04-18 23:47:39 -0400

    Hardcode institutions on the map

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import { Link } from 'gatsby'
 const Home = () => <div>
   <h1>Home</h1>
 	<p>You are now logged in! <Link to="/app/profile">View profile</Link></p>
+	<p>View the <Link to="/app/map">Map</Link></p>
 	</div>
 
 export default Home
--- src/pages/app.js
+++ src/pages/app.js
@@ -3,6 +3,7 @@ import { Router } from "@reach/router"
 import Layout from "../components/layout"
 import Details from "../components/Details"
 import Home from "../components/Home"
+import Map from "../components/Map"
 import Login from "../components/Login"
 import SignUp from "../components/SignUp"
 import PrivateRoute from "../components/PrivateRoute"
@@ -13,6 +14,7 @@ const App = () => (
     <Router>
       <PrivateRoute path="/app/home" component={Home} />
       <PrivateRoute path="/app/profile" component={Details} />
+      <PrivateRoute path="/app/map" component={Map} />
       <Login path="/app/login" />
       <SignUp path="/app/signup" />
     </Router>
