62333c3fda9b98193d7044b4357d0319bf35da14
jzhang005 <jzhang005@citymail.cuny.edu>
2019-04-30 14:06:51 -0400

    Fix Tab component, need to fix funciton showTabHandler in rightMenu

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './rightMenu.css';
 
 const tab = (props) => {
-    return (
-        <span>
-            <button className="tabMenuItem" onClick={props.click}>{props.title}</button>
+    let tabContent = null;
+    if (props.click === 'true') {
+        tabContent = (
             <div className="tabContent">
                 {props.content}
             </div>
+        );
+    }
+
+    return (
+        <span>
+            <button type='button' className="tabMenuItem" onClick={props.click}>{props.title}</button>
+            {tabContent}
         </span>
     );
 };
--- src/components/rightMenu.jsx
+++ src/components/rightMenu.jsx
@@ -1,99 +1,67 @@
-import React, { Component } from 'react'
-import './rightMenu.css'
-import Tab from './Tab'
+import React, { Component } from 'react';
+import './rightMenu.css';
+import Tab from './Tab';
 
 class rightMenu extends Component {
   state = {
     tabs: [
-      { id: '0', title: 'Setting⚙️', click: false, conten: 'set' },
-      { id: '1', title: 'List', click: false, content: 'list' },
-      { id: '2', title: 'Discussion', click: false, content: 'discuss' },
-    ],
+      { id: '0', title: 'Setting⚙️', content: 'set' },
+      { id: '1', title: 'List', content: 'list' },
+      { id: '2', title: 'Discussion', content: 'discuss' },
+    ]
     // other states if needed
-  }
+  };
 
   showTabHandler = (id) => {
+    // get all the tabs in state
     let tabs = [...this.state.tabs];
+    // get the clicked tab index
     let showTabIndex = tabs.findIndex((t) => t.id === id);
+    // get the clicked tab by showTabIndex
     let tab = {...this.state.tabs[showTabIndex]};
 
-    if (tab.click) {
+    // if the tab is already open then do nothing
+    if (tab.click === 'true') {
       // do nothing
-    } else {
+      //console.log(tab.click);
+    } else { // else clicked tab.click = true; other tabs are false
+      tab.click = 'true';
+
       tabs.forEach(function(t) {
         if (t.id !== id) {
-          t.click = false
-        }
+          t.click = 'false';
+        };
       });
-      tab.click = true;
-
+      
       tabs[showTabIndex] = tab;
+      console.log(tabs);
       
       this.setState({ tabs: tabs });
     }
   }
 
   render() {
+    let tabs = null;
+
+    tabs = (
+      <div>
+        {this.state.tabs.map((tab, index) => {
+          return <Tab 
+            key={tab.id}
+            title={tab.title}
+            click={this.showTabHandler(index)}
+            content={tab.content}
+            />
+        })}
+      </div>
+    );
+
     return (
       <div className="menuContainer">
-        <div className="tabMenu">
-          <Tab
-            click={this.showTabHandler(this.state.tabs[0])}
-            key={this.state.tabs[0].id}
-            title={this.state.tabs[0].title}
-            content={this.state.tabs[0].content}
-          />
-          <Tab
-            click={this.showTabHandler(this.state.tabs[1])}
-            key={this.state.tabs[1].id}
-            title={this.state.tabs[1].title}
-            content={this.state.tabs[1].content}
-          />
-          <Tab
-            click={this.showTabHandler(this.state.tabs[2])}
-            key={this.state.tabs[2].id}
-            title={this.state.tabs[2].title}
-            content={this.state.tabs[2].content}
-          />
-          {/* <button className="tabMenuItem" onClick={this.showTabHandler}>⚙️</button>
-                    <button className="tabMenuItem" onClick={this.showTabHandler}>List</button>
-                    <button className="tabMenuItem" onClick={this.showTabHandler}>Discuss</button> */}
-        </div>
-
-        <div className="tabContent">
-          <div className="tabSetting" id="set">
-            Settings
-          </div>
-          <div className="tabList" id="list">
-            <p>Air Quality</p>
-            <p>Health Incidents</p>
-            <p>Cultural Sites</p>
-            <p>Vehicle Idlling Complaints</p>
-            <p>Vehicle Collisions</p>
-          </div>
-
-          <div className="tabDiscuss" id="discuss">
-            <div>
-              <h5>Comments</h5>
-              <div className="discussContainer">
-                <img src="" alt="Image" />
-                <p>Comments</p>
-                <span>mm/dd/yy</span>
-              </div>
-              <div className="discussContainer">
-                <img src="" alt="Image" />
-                <p>Comments</p>
-                <span>mm/dd/yy</span>
-              </div>
-            </div>
-            <div>
-              <h5>Requests</h5>
-            </div>
-          </div>
-        </div>
+        {tabs}
       </div>
-    )
+    );
   }
 }
 
-export default rightMenu
+export default rightMenu;
