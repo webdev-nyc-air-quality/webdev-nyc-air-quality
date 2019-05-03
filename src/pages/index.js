b28ffe1711e31e1b1438cbfcaf2b622edf629729
jzhang005 <jzhang005@citymail.cuny.edu>
2019-05-02 22:59:11 -0400

    Change styles for main page UI
    Includes: 1. Center the header 2. Add dataset title under the header 3. Remove unnecessary styles and code in layout.css and layout.js 4. Position the map on the left with width of 65% of the screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Map from '../components/Map'
 import Amplify from 'aws-amplify'
@@ -8,12 +7,6 @@ Amplify.configure(config)
 
 const IndexPage = () => (
   <Layout>
-    <h1>Welcome</h1>
-    <p>Welcome to our website!</p>
-    <p>Create a new account: <Link to="/app/signup">Sign Up</Link></p>
-    <Link to="/app/login">Sign In</Link><br />
-    <Link to="/app/home">Home</Link><br />
-    <Link to="/app/profile">Your profile</Link>
     <Map />
   </Layout>
 )
