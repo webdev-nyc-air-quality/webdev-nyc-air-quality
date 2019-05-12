d3fa2e52d1e139abeb26c4d5f17d44ecdbefd788
mtadjdine@yahoo.com <mtadjdine@yahoo.com>
2019-05-12 15:03:18 -0400

    Add geocode_experiment page

---
+++ src/pages/geocode_experiment.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const GeocodeExperimentPage = () => (
+  <Layout>
+     Geocode Experiment
+  </Layout>
+)
+
+export default GeocodeExperimentPage
