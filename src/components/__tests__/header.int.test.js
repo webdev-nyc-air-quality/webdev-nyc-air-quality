32d373639c289ccf4a1358ecd6dbc5f65a503728
Ian S. McBride <ian.s.mcb@gmail.com>
2019-05-12 10:06:23 -0400

    Add two more integration tests for header

--- src/components/__tests__/header.int.test.js
+++ src/components/__tests__/header.int.test.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { render, cleanup } from 'react-testing-library'
 
 import Header from '../header'
+import { isLoggedIn }from '../../utils/auth'
+
+jest.mock('../../utils/auth')
 afterEach(cleanup)
 
 describe('Header component', () => {
@@ -10,5 +13,20 @@ describe('Header component', () => {
     const brand = container.querySelector('.navbar-brand')
     expect(brand).not.toBeNull()
     expect(brand.textContent).toBe('Neighborhood Data')
+  })
+
+  test('should show register, login links when logged out', () => {
+    isLoggedIn.mockReturnValue(false)
+    const { container } = render(<Header />)
+    const items = container.querySelectorAll('.dropdown-menu .dropdown-item')
+    expect(items[0].textContent).toBe('Register')
+    expect(items[1].textContent).toBe('Login')
+  })
+
+  test('should show logout link when logged in', () => {
+    isLoggedIn.mockReturnValue(true)
+    const { container } = render(<Header />)
+    const items = container.querySelectorAll('.dropdown-menu .dropdown-item')
+    expect(items[0].textContent).toBe('Logout')
   })
 })
