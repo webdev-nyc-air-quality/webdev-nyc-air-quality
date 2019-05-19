15f591b1ad2e93cb33ba6b39e1f86c2ed514df6d
Ian S. McBride <ian.s.mcb@gmail.com>
2019-05-19 17:32:51 -0400

    Remove unwanted Map route

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Router } from '@reach/router'
+
 import Layout from '../components/layout'
 import Details from '../components/Details'
 import Home from '../components/Home'
-import Map from '../components/Map'
 import Login from '../components/Login'
 import SignUp from '../components/SignUp'
 import PrivateRoute from '../components/PrivateRoute'
@@ -13,7 +13,6 @@ const App = () => (
     <Router>
       <PrivateRoute path='/app/home' component={Home} />
       <PrivateRoute path='/app/profile' component={Details} />
-      <PrivateRoute path='/app/map' component={Map} />
       <Login path='/app/login' />
       <SignUp path='/app/signup' />
     </Router>
