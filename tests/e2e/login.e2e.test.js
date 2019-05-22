f04dcd31ddb717f2cc2dc0cb5a473a3710c6fccb
Ian S. McBride <ian.s.mcb@gmail.com>
2019-05-22 18:21:00 -0400

    Fix initial E2E test
    Previously, it checked google.com, not our app.

--- tests/e2e/login.e2e.test.js
+++ tests/e2e/login.e2e.test.js
@@ -1,11 +1,14 @@
 describe('E2E tests', () => {
+  const url = 'https://master.d2mdn3yvvbw50z.amplifyapp.com'
   beforeAll(async () => {
-    await page.goto('https://google.com')
+    await page.goto(url)
   })
 
-  it('should be titled "Google"', async () => {
-    await expect(page.title()).resolves.toMatch('Google')
+  it('should be titled "NYC Data Speaks"', async () => {
+    await expect(page.title()).resolves.toEqual(expect.stringContaining('NYC Data Speaks'))
   })
 
-  test('should login successfully', () => {})
+  test('should login successfully', () => {
+   
+  })
 })
