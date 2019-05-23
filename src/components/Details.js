0956dd5f3f3a74b11df9812efe4b4fdf707abb34
jzhang005 <jzhang005@citymail.cuny.edu>
2019-05-23 16:29:30 -0400

    Change profile page style

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import { getCurrentUser } from '../utils/auth'
+import { Row, Col } from 'react-bootstrap'
 
 const Home = () => {
+  const styles = {
+    profileContainer: {
+      width: '80%',
+      margin: '1rem auto',
+      border: '1px solid lightgray',
+      borderRadius: '5px',
+      padding: '0.5rem',
+    },
+  }
+
   const user = getCurrentUser()
-  console.log('user:', user)
   return (
-    <div>
-      <h1>Profile Details</h1>
-      <p>Email: {user.email}</p>
-      <p>Phone: {user.phone_number}</p>
-      <p>Username: {user.username}</p>
-    </div>
+    <Row style={styles.profileContainer}>
+      <Col>
+        <h1>Profile Details</h1>
+        <p>Email: {user.email}</p>
+        <p>Phone: {user.phone_number}</p>
+        <p>Username: {user.username}</p>
+      </Col>
+    </Row>
   )
 }
 
